c2ff24ebd51575f97f659f895e44049e592c77f8
DiorAbjalilov <abjalilovdiyor1@gmail.com>
2021-07-12 22:49:56 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,46 +1,9 @@
-import React, { Component } from "react";
-import reactDom from "react-dom";
+import React from "react";
 
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
-//   componentDidMount() {
-//     this.timerID = setInterval(() => {
-//       this.tick();
-//     }, 1000);
-//     console.log("didm");
-//   }
-//   componentWillMount() {
-//     clearInterval(this.timerID);
-//     console.log("wilm");
-//   }
-//   tick() {
-//     this.setState({
-//       date: new Date(),
-//     });
-//     console.log("tick");
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>hozirgi vaqt: {this.state.date.toLocaleTimeString()}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-export default class App extends Component {
-  short = (sound, ev) => {
-    alert(sound + ev.type);
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={(ev) => this.short("BEEP!", ev)}>Signal Click</button>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
